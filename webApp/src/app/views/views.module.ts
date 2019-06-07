7b939c89d6d5ae3cbcceb31937204d96152d37c5
Pavel Chachotkin <pavel.chachotkin@ROTTERDAM.FORM4>
2019-06-07 12:27:06 +0200

    event-site

--- webApp/src/app/views/views.module.ts
+++ webApp/src/app/views/views.module.ts
@@ -4,29 +4,38 @@ import { CommonModule } from '@angular/common';
 
 import { CardComponent } from './card/card.component';
 import { BlogCardComponent } from './blog-card/blog-card.component';
+import { EventCardComponent } from './event-card/event-card.component';
 
 //Material imports
 import { 
   MatCardModule, 
-  MatButtonModule
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule,
+  MatIconModule
 
 } from '@Angular/material';
+
 
 
 @NgModule({
   declarations: [
     CardComponent, 
-    BlogCardComponent
+    BlogCardComponent, EventCardComponent
   ],
   imports: [
     CommonModule,
     //Material
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatIconModule
   ],
   exports: [
     CardComponent,
-    BlogCardComponent
+    BlogCardComponent,
+    EventCardComponent
   ]
 })
 export class ViewsModule { }
